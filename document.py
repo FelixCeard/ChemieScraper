374cab3a8624bf04caa7e7755826fd6ed9c0531a
Felix Ceard-Falkenberg <felix@falkenbergs.de>
2022-08-02 21:08:58 +0200

    linux2

--- document.py
+++ document.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 
 from docx import Document
 from docx.shared import Inches
--- name_scraper.py
+++ name_scraper.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+# -*- coding: utf-8 -*-
 import time
 
 from selenium.common.exceptions import TimeoutException
